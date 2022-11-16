245b3ac3b65b1ca18da55f1eb562d7945948f843
Murat Dogan <forasle@gmail.com>
2022-11-16 23:41:39 +0300

    Create Employee Command Request is added.

---
+++ Eah.WorkSafety.WebApp.Back/Core/Application/Features/CQRS/Commands/CreateEmployeeCommandRequest.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+
+namespace Eah.WorkSafety.WebApp.Back.Core.Application.Features.CQRS.Commands
+{
+    public class CreateEmployeeCommandRequest:IRequest
+    {
+        public int Id { get; set; }
+
+        public string? IdentificationNumber { get; set; }
+
+        public string? RegistrationNumber { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Surname { get; set; }
+
+        public string? Position { get; set; }
+
+        public string? Department { get; set; }
+
+        public DateTime? StartDateOfEmployment { get; set; }
+
+        public string? Address { get; set; }
+    }
+}
---
+++ Eah.WorkSafety.WebApp.Back/Core/Application/Features/CQRS/Handlers/Commands/CreateEmployeeCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Eah.WorkSafety.WebApp.Back.Core.Application.Features.CQRS.Commands;
+using Eah.WorkSafety.WebApp.Back.Core.Application.Interfaces;
+using Eah.WorkSafety.WebApp.Back.Core.Domain;
+using MediatR;
+
+namespace Eah.WorkSafety.WebApp.Back.Core.Application.Features.CQRS.Handlers.Commands
+{
+    public class CreateEmployeeCommandHandler : IRequestHandler<CreateEmployeeCommandRequest>
+    {
+        private readonly IRepository<Employee> repository;
+
+        public CreateEmployeeCommandHandler(IRepository<Employee> repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<Unit> Handle(CreateEmployeeCommandRequest request, CancellationToken cancellationToken)
+        {
+            await this.repository.CreateAsync(
+                new Employee
+                {
+                    IdentificationNumber = request.IdentificationNumber,
+                    RegistrationNumber = request.RegistrationNumber,
+                    Name = request.Name,
+                    Surname = request.Surname,
+                    Position = request.Position,
+                    Department = request.Department,
+                    StartDateOfEmployment = request.StartDateOfEmployment,
+                    Address = request.Address
+                }
+                );
+            return Unit.Value;
+        }
+    }
+}
