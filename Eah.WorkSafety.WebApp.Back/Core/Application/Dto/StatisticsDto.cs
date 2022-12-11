497b79e4859ae46ffdbad11903371b95e16a5c48
Murat Dogan <forasle@gmail.com>
2022-12-12 00:51:21 +0300

    Statistics endpoint is added.

---
+++ Eah.WorkSafety.WebApp.Back/Core/Application/Dto/StatisticsDto.cs
@@ -0,0 +1,31 @@
+﻿namespace Eah.WorkSafety.WebApp.Back.Core.Application.Dto
+{
+    public class StatisticsDto
+    {
+
+        public StatisticsDto(int numberOfAccidents, DateTime? lastAccidentDate, int numberOfNearMisses, int numberOfRiskAssessments, int numberOfInconsistencies, int numberOfContingencyPlans, int numberOfPreventiveActivities)
+        {
+            NumberOfAccidents = numberOfAccidents;
+            LastAccidentDate = lastAccidentDate;
+            NumberOfNearMisses = numberOfNearMisses;
+            NumberOfRiskAssessments = numberOfRiskAssessments;
+            NumberOfInconsistencies = numberOfInconsistencies;
+            NumberOfContingencyPlans = numberOfContingencyPlans;
+            NumberOfPreventiveActivities = numberOfPreventiveActivities;
+        }
+
+        public int NumberOfAccidents { get; set; }
+
+        public DateTime? LastAccidentDate { get; set; }
+
+        public int NumberOfNearMisses { get; set; }
+
+        public int NumberOfRiskAssessments { get; set; }
+
+        public int NumberOfInconsistencies { get; set; }
+
+        public int NumberOfContingencyPlans { get; set; }
+
+        public int NumberOfPreventiveActivities { get; set; }
+    }
+}
---
+++ Eah.WorkSafety.WebApp.Back/Core/Application/Features/CQRS/Handlers/Queries/GetAllStatisticsQueryHandler.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Eah.WorkSafety.WebApp.Back.Core.Application.Dto;
+using Eah.WorkSafety.WebApp.Back.Core.Application.Features.CQRS.Queries;
+using Eah.WorkSafety.WebApp.Back.Core.Application.Interfaces;
+using Eah.WorkSafety.WebApp.Back.Core.Domain;
+using MediatR;
+
+namespace Eah.WorkSafety.WebApp.Back.Core.Application.Features.CQRS.Handlers.Queries
+{
+    public class GetAllStatisticsQueryHandler : IRequestHandler<GetAllStatisticsQueryRequest, StatisticsDto>
+    {
+        private readonly IRepository<Accident> accidentRepository;
+        private readonly IRepository<NearMiss> nearMisstRepository;
+        private readonly IRepository<RiskAssessment> riskAssesmentRepository;
+        private readonly IRepository<Inconsistency> inconsistencyRepository;
+        private readonly IRepository<ContingencyPlan> contingencyPlanRepository;
+        private readonly IRepository<PreventiveActivity> preventiveActivityRepository;
+        private readonly IMapper mapper;
+
+        public GetAllStatisticsQueryHandler(IRepository<Accident> accidentRepository, IRepository<NearMiss> nearMisstRepository, IRepository<RiskAssessment> riskAssesmentRepository, IRepository<Inconsistency> inconsistencyRepository, IRepository<ContingencyPlan> contingencyPlanRepository, IRepository<PreventiveActivity> preventiveActivityRepository, IMapper mapper)
+        {
+            this.accidentRepository = accidentRepository;
+            this.nearMisstRepository = nearMisstRepository;
+            this.riskAssesmentRepository = riskAssesmentRepository;
+            this.inconsistencyRepository = inconsistencyRepository;
+            this.contingencyPlanRepository = contingencyPlanRepository;
+            this.preventiveActivityRepository = preventiveActivityRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<StatisticsDto> Handle(GetAllStatisticsQueryRequest request, CancellationToken cancellationToken)
+        {
+            var countOfAccident = await this.accidentRepository.GetAllCount();
+            Accident? lastAccident = await this.accidentRepository.GetByFilterAsync(x=>x.Date!);
+            var countOfNearMisses = await this.nearMisstRepository.GetAllCount();
+            var countOfRiskAssessments = await this.riskAssesmentRepository.GetAllCount();
+            var countOfInconsistencies = await this.nearMisstRepository.GetAllCount();
+            var countOfContingencyPlans = await this.contingencyPlanRepository.GetAllCount();
+            var countOfPreventiveActivities = await this.preventiveActivityRepository.GetAllCount();
+            var result = new StatisticsDto(countOfAccident, lastAccident.Date!, countOfNearMisses, countOfRiskAssessments, countOfInconsistencies, countOfContingencyPlans, countOfPreventiveActivities
+                );
+            return result;
+        }
+    }
+}
