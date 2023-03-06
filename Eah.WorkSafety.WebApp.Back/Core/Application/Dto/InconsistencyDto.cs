dc90689f0db9170322f2280221062c7038caafd1
Murat Doğan <melank0lik@hotmail.com>
2023-03-06 10:47:27 +0300

    fix some endpoints

--- Eah.WorkSafety.WebApp.Back/Core/Application/Dto/InconsistencyDto.cs
+++ Eah.WorkSafety.WebApp.Back/Core/Application/Dto/InconsistencyDto.cs
@@ -6,13 +6,15 @@ namespace Eah.WorkSafety.WebApp.Back.Core.Application.Dto
     {
         public int Id { get; set; }
 
-        public string? ReferenceNumber { get; set; }
+        public string? SceneOfInconsistency { get; set; }
 
         public string? Information { get; set; }
 
         public int CreatorUserId { get; set; }
 
-        public DateTime? Date { get; set; }
+        public DateTime InconsistencyDate { get; set; }
+
+        public DateTime? CreationDate { get; set; }
 
         public bool RootCauseAnalysisRequirement { get; set; }
 
